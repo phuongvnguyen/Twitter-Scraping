90ca3bc5a2178a477921ec0385f04f0a86dc5887
phuongvnguyen <31282453+phuongvnguyen@users.noreply.github.com>
2020-05-30 20:36:41 +0200

    Update Twitter_Scraper.py

--- Twitter_Scraper.py
+++ Twitter_Scraper.py
@@ -4,6 +4,8 @@ or "der Immobilien" in German, from the Twitter network.
 In this program, we specify the time period from 2017 April 24th to 28th. 
 However, One might modify his/her own time periods.
 After running this program, the excel and CSV files are produced
+Programmer: Phuong V. Nguyen
+phuong.nguyen@economics.uni-kiel.de
 """
 
 from twitterscraper import query_tweets
